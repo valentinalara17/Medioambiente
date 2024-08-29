28b42470982481aaa9ea804badbb11c3609e2106
canndec <106876077+canndec@users.noreply.github.com>
2024-08-29 17:55:14 -0300

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -1,6 +1,8 @@
 const dondeSurgen = document.getElementById("donde-Surge");
 
 dondeSurgen.addEventListener("mouseover", () => {
-    dondeSurgen.style.backgroundColor rgb (73,86,61);
-    
+    dondeSurgen.style.backgroundColor = "rgb (73,86,61)";
 });
+dondeSurgen.addEventListener('mouseout', () => {
+        dondeSurgen.style.backgroundColor = "white"; 
+        });
