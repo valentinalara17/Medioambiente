55a5ff76a66547b2d55d35143a0748c18727bacf
canndec <106876077+canndec@users.noreply.github.com>
2024-09-09 16:40:27 -0300

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -1,16 +1,6 @@
 const dondeSurgen = document.getElementById("donde-Surge");
 
 dondeSurgen.addEventListener("mouseover", () => {
-    dondeSurgen.style.backgroundColor = "rgb(150,240,149)";
-});
-dondeSurgen.addEventListener('mouseout', () => {
-        dondeSurgen.style.backgroundColor = "white"; 
-        });
-
-
-const queHacer = document.getElementById("que-hacer");
-
-dondeSurgen.addEventListener("mouseover", () => {
     dondeSurgen.style.backgroundColor = "rgb(110,240,149)";
 });
 dondeSurgen.addEventListener('mouseout', () => {
@@ -18,11 +8,21 @@ dondeSurgen.addEventListener('mouseout', () => {
         });
 
 
-const accionesEmpresas = document.getElementById("acciones-empresas");
+const queHacer = document.getElementById("que-Hacer");
 
-dondeSurgen.addEventListener("mouseover", () => {
-    dondeSurgen.style.backgroundColor = "rgb(110,240,149)";
+queHacer.addEventListener("mouseover", () => {
+    queHacer.style.backgroundColor = "rgb(110,240,149)";
 });
-dondeSurgen.addEventListener('mouseout', () => {
-        dondeSurgen.style.backgroundColor = "white"; 
+queHacer.addEventListener('mouseout', () => {
+        queHacer.style.backgroundColor = "white"; 
+        });
+
+
+const accionesEmpresas = document.getElementById("acciones-Empresas");
+
+accionesEmpresas.addEventListener("mouseover", () => {
+    accionesEmpresas.style.backgroundColor = "rgb(110,240,149)";
+});
+accionesEmpresas.addEventListener('mouseout', () => {
+        accionesEmpresas.style.backgroundColor = "white"; 
         });
