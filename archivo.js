636b60bbeccc3327031d05e78a4bf269c85725c0
canndec <106876077+canndec@users.noreply.github.com>
2024-09-09 16:36:29 -0300

    Update archivo.js

--- archivo.js
+++ archivo.js
@@ -1,7 +1,27 @@
 const dondeSurgen = document.getElementById("donde-Surge");
 
 dondeSurgen.addEventListener("mouseover", () => {
-    dondeSurgen.style.backgroundColor = rgb(110,240,149);
+    dondeSurgen.style.backgroundColor = "rgb(150,240,149)";
+});
+dondeSurgen.addEventListener('mouseout', () => {
+        dondeSurgen.style.backgroundColor = "white"; 
+        });
+
+
+const queHacer = document.getElementById("que-hacer");
+
+dondeSurgen.addEventListener("mouseover", () => {
+    dondeSurgen.style.backgroundColor = "rgb(110,240,149)";
+});
+dondeSurgen.addEventListener('mouseout', () => {
+        dondeSurgen.style.backgroundColor = "white"; 
+        });
+
+
+const accionesEmpresas = document.getElementById("acciones-empresas");
+
+dondeSurgen.addEventListener("mouseover", () => {
+    dondeSurgen.style.backgroundColor = "rgb(110,240,149)";
 });
 dondeSurgen.addEventListener('mouseout', () => {
         dondeSurgen.style.backgroundColor = "white"; 
